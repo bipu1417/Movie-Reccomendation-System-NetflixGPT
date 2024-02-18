f7bb8bf1a696f3cf3e7a27681bd8cd8ef11ed276
Biplab Mahato <bips4lcs@gmail.com>
2024-02-18 13:39:35 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,13 +2,14 @@ export const logo = 'https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897
 export const photoUrl = "https://avatars.githubusercontent.com/u/15422942?v=4";
 export const userIcon = "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png";
 
-export const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYjJhZWJkYmZiZmJjZDk1MGI5YWYwNWRlN2FhOWFhNiIsInN1YiI6IjY1ZDBkMjMyNjJmY2QzMDE4NzQxNTQ0NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.M8MkCcdGS8J53L5_BaDa1W5kWlF0yCuhbM6pEukwQC0'
-    }
-  };
+//Uncomment the below lines to run this project successfully
+// export const options = {
+//     method: 'GET',
+//     headers: {
+//       accept: 'application/json',
+//       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYjJhZWJkYmZiZmJjZDk1MGI5YWYwNWRlN2FhOWFhNiIsInN1YiI6IjY1ZDBkMjMyNjJmY2QzMDE4NzQxNTQ0NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.M8MkCcdGS8J53L5_BaDa1W5kWlF0yCuhbM6pEukwQC0'
+//     }
+//   };
 
   export const url = 'https://api.themoviedb.org/3/movie/now_playing?page=1';
   export const MOVIE_VIDEO_URL = "https://api.themoviedb.org/3/movie/";
